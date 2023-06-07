40489fca7187eeb5fcbf0fcefcdffe21c4b7eb57
Hydronio <aldrynlopez21@gmail.com>
2023-06-07 11:12:30 -0400

    connect with metamask service created

--- components/modals/connect.vue
+++ components/modals/connect.vue
@@ -10,7 +10,7 @@
 
     <v-sheet class="grid" color="transparent">
       <v-btn plain @click="connectMetamask()">
-        <img src="~/assets/sources/logos/sender-icon.svg" alt="near">
+        <img src="~/assets/sources/logos/metamask-icon.svg" alt="near">
         
         <div class="divcol astart" style="gap: 5px">
           <span class="h12_em bold">metamask</span>
@@ -42,7 +42,7 @@ export default {
   },
   methods: {
     async connectMetamask() {
-        console.log(this.$metamask.userAccounts)
+        console.log(this.$metamask.userAccount)
       if (this.$metamask.haveMetamask()) {
         await this.$metamask.init()
         console.log(this.$metamask.userAccounts)
