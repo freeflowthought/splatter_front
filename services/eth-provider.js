4534978330a470d77632e3364331a9ed140f8009
Hydronio <aldrynlopez21@gmail.com>
2023-06-08 14:33:54 -0400

    .

--- services/eth-provider.js
+++ services/eth-provider.js
@@ -1,4 +1,18 @@
 import Vue from 'vue'
+const { ethers } = require("ethers");
+//const { getAddress } = require("ethers/lib/utils");
+const metaMaskProvider = new ethers.providers.Web3Provider(this.$metamask.$ethereum)
+//mumbaqi testnet 80001
+//scroll alpha 534353
+const defaultProvider = new ethers.getDefaultProvider(80001);
 
+const ethersProvider = {
+    chainId : undefined,
+    defaultProvider : defaultProvider,
+    metaMaskProvider : metaMaskProvider,
 
-Vue.prototype.$ethProvider = {}
+    
+}
+
+Vue.prototype.$provider = ethersProvider
+
