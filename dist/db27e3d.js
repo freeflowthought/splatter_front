99042f8d44e263b3cb34d01898cd12134daeb6f1
Hydronio <aldrynlopez21@gmail.com>
2023-11-07 14:24:39 -0400

    build

--- dist/db27e3d.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{858:function(n,e,o){"use strict";o.r(e),e.default={pageNotFound:"La página no existe",otherError:"Ha ocurrido un error",buttonTitle:"Página de inicio",successMessage:"Transacción completa. Ver tx!",warningMessage:"Transacción pendiente",cancelMessage:"Transacción fallida. Inténtalo de nuevo"}}}]);
